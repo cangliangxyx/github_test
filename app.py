2b57c81d1b3cd124ba3c369d1cfc54726e83b699
vendor.liang.chang <changliang9897@gmail.com>
2024-11-08 11:28:22 +0800

    123123

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+import os
+os.environ['config_env'] = "localhost"
+app = Flask(__name__)
+print(app)
+
+@app.route("/")
+def index():
+    flask_data = 'infra-数据接口,运行环境:%s' % (os.environ.get('config_env'))
+    return render_template('index.html', flask_data=flask_data)
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8090,
+        debug=True
+    )
